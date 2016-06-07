514d7030a9f12b7f1c36c817b547b628640c5b4a
marvelrock <thuang1020@163.com>
2016-06-07 10:34:32 +0800

    bugfix:下载submodules

--- tools/download.sh
+++ tools/download.sh
@@ -9,6 +9,8 @@ fi
 
 git clone https://github.com/eflagcomm/${repo_name}.git
 cd $repo_name
+git submodule init
+git submodule update
 ./tools/git-archive-all/git_archive_all.py ${repo_name}.tar.gz
 cd $old_dir
 mv ${repo_name}/${repo_name}.tar.gz .
